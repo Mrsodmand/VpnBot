33516f7d8b037b8acf2858af8abe671a84e24903
Mahdi Soodmand - Mac <mahdi_sodmand@yahoo.com>
2026-06-13 16:51:41 +0330

    fix join to channel

--- app/Http/Controllers/Api/TelegramBotController.php
+++ app/Http/Controllers/Api/TelegramBotController.php
@@ -783,7 +783,7 @@ class TelegramBotController extends Controller
 
                 $result = $this->telegramSdk->getChatMember($data);
                 if (array_key_exists('ok', $result) && $result['ok'] == true) {
-                    $status = $res['result']['status'] ?? null;
+                    $status = $result['result']['status'] ?? null;
                     if (in_array($status, ['member', 'administrator', 'creator'])){
                         return $this->isJoined = true;
                     }
